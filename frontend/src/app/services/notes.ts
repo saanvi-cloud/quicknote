c6b1cfbc3e83b5a7cdf51233259c458e1f953a16
saanvi-cloud <saanvidd2003@gmail.com>
2026-01-28 02:32:50 -0800

    Updated for deploying to frontend

--- frontend/src/app/services/notes.ts
+++ frontend/src/app/services/notes.ts
@@ -1,12 +1,15 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environments';
+
 
 @Injectable({ providedIn: 'root' })
 export class NotesService {
 
   // private apiUrl = 'http://localhost:3000/api/notes';
-  private apiUrl = 'https://quicknote-yzl7.onrender.com/api/notes';
+  // private apiUrl = 'https://quicknote-yzl7.onrender.com/api/notes';
+  private apiUrl = environment.apiUrl;
 
 
   constructor(private http: HttpClient) {}
---
+++ frontend/src/environments/environments.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://quicknote-yzl7.onrender.com/api/notes'
+};
