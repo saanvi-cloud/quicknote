9fc6dba4fe4e18216fe267e4a689cb0151933720
saanvi-cloud <saanvidd2003@gmail.com>
2026-01-28 01:01:03 -0800

    added gitignore to backend and updated notes.ts

--- frontend/src/app/services/notes.ts
+++ frontend/src/app/services/notes.ts
@@ -5,7 +5,9 @@ import { Observable } from 'rxjs';
 @Injectable({ providedIn: 'root' })
 export class NotesService {
 
-  private apiUrl = 'http://localhost:3000/api/notes';
+  // private apiUrl = 'http://localhost:3000/api/notes';
+  private apiUrl = 'https://quicknote-yzl7.onrender.com/api/notes';
+
 
   constructor(private http: HttpClient) {}
 
